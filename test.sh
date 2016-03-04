1463f4d859180147a2c2df69d9a4b73fff05051f
unnumbered <unnumbered.ab@gmail.com>
2016-03-03 20:23:55 -0500

    blah

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm test/out/*.txt
+
+for input in test/in/*.txt
+do
+  echo "==========================================="
+  reference=${input/in/out_ref}
+  output=${input/in/out}
+  echo "python3 translator.py $input $output"
+  python3 translator.py $input $output > /dev/null
+  echo "diff $reference $output"
+  diff $reference $output
+  echo "==========================================="
+  echo
+done
