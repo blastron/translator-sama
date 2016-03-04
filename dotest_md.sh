a267f33e3cf091f623857d3b40d66ffe8050d0ee
unnumbered <unnumbered.ab@gmail.com>
2016-03-04 17:26:30 -0500

    add

---
+++ dotest_md.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm test/out/*.txt
+
+for input in test/in/*.txt
+do
+  echo "==========================================="
+  reference=${input/in/out_ref_md}
+  reference_md=${reference/txt/md}
+  output=${input/in/out_md}
+  output_md=${output/txt/md}
+  echo "python3 translator.py $input $output_md"
+  python3 translator.py $input $output_md > /dev/null
+  echo "diff $reference_md $output_md"
+  diff $reference_md $output_md
+  echo "==========================================="
+  echo
+done
